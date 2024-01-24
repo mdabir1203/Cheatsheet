d520c17455acb9c0f233e40d6a1216dd97e2c82e
Mohammad Abir Abbas <md.abir1203@gmail.com>
2024-01-24 05:49:53 +0100

    Fixed headers, added colors

--- animation.js
+++ animation.js
@@ -1,1 +1,15 @@
-gsap.fromTo('.animated-text', { y: '100%' }, { y: '0%', duration: 1, repeat: -1, yoyo: true });
+const words = ["CHEATSHEET", "INTERVIEW PHRASES"]
+
+let cursor = gsap.to('.cursor', {opacity:0, ease: "power2.inOut", repeat:-1})
+let masterTl = gsap.timeline({repeat: -1}).pause()
+let boxTl = gsap.timeline()
+
+boxTl.to('.box', {duration:1, width:"1vw", delay: 1, ease: "power4.inOut"})
+  .from('.hi', {duration:1, y:"7vw", eas: "power3.out"})
+  .to('.box', {duration:1, height:"7vw", ease: "elastic.out", onComplete: () => masterTl.play() })
+  .to('.box', {duration:2, autoAlpha:0.7, yoyo: true, repeat: -1, ease:"rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})"})
+words.forEach(word => {
+  let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay:1})
+  tl.to('.text', {duration: 1, text: word})
+  masterTl.add(tl)
+})
